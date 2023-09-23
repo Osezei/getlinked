4b564e827e70a4760774b3f75f69557685ab25f7
John Osezei Ikhimioya <john.osezei@yahoo.com>
2023-09-23 23:31:13 +0100

    final

--- app/register/page.js
+++ app/register/page.js
@@ -6,27 +6,40 @@ import Image from "next/image";
 import Walk from "../../public/walking1.png";
 import Walk2 from "../../public/walking2.png";
 import Modal from "../../components/Modal";
-import Modall from "../../components/Modall";
 
 const Register = () => {
-  const [show, setShow] = useState(false);
   const activePage = "Contact";
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
   return (
     <Layout activePage={activePage}>
-      <section className="text-white flex justify-center gap-x-3">
-        {/* <Modall /> */}
+      <section className="text-white flex flex-col md:flex-row justify-center md:gap-x-3 relative mb-3 md:mb-0">
         <Image
           src={Designer}
           width={717}
           height={717}
-          alt="designer overflow-hidden"
+          alt="designer "
+          className="overflow-hidden hidden md:block"
         />
-        <div className="w-[740px] h-[740px] contact-page flex justify-center">
-          <div className="mt-[65px]">
-            <h1 className="text-[#D434FE] text-[32px] font-semibold">
+        <h1 className="text-[#D434FE] text-[20px] font-semibold md:hidden w-[263px] mx-auto">
+          Register
+        </h1>
+        <Image
+          src={Designer}
+          width={195}
+          height={155}
+          alt="designer "
+          className="object-fit overflow-hidden md:hidden mx-auto"
+        />
+        <div className="w-[263px] mx-auto md:w-[740px] md:h-[740px] contact-page flex justify-center">
+          <div className="md:mt-[65px]">
+            <h1 className="text-[#D434FE] text-[32px] font-semibold hidden md:block">
               Register
             </h1>
-            <div className="mt-[46px] mb-[19px] relative">
+            <div className="mt-[23px] md:mt-[46px] md:mb-[19px] relative">
               <p className="">
                 Be part of this movement!
                 <span className="text-[#D434FE] "> .....................</span>
@@ -36,53 +49,54 @@ const Register = () => {
                 <Image src={Walk2} alt="walk1" width={30} height={30} />
               </div>
             </div>
-            <p className="text-[24px] mb-[33px]">CREATE YOUR ACCOUNT</p>
-            <form className="">
-              <div className="grid grid-cols-2 gap-x-[32px] gap-y-[29px]">
+            <p className="text-[20px] mb-[23px] md:text-[24px] md:mb-[33px]">
+              CREATE YOUR ACCOUNT
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="text-[13px] md:text-[14px]"
+            >
+              <div className="grid grid-flow-row md:grid-cols-2 gap-y-[18px] md:gap-x-[32px] md:gap-y-[29px]">
                 {" "}
                 <div>
-                  <p className="mb-[11px]">{`Teams's Name`}</p>
+                  <p className="mb-[6px] md:mb-[11px]">{`Teams's Name`}</p>
                   <input
                     type="text"
-                    required
                     name="team-name"
                     placeholder="Enter the name of your group"
-                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[47px]"
+                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[39px] md:h-[47px]"
                   />
                 </div>
                 <div>
-                  <p className="mb-[11px]">{`Phone Number`}</p>
+                  <p className="mb-[6px] md:mb-[11px]">{`Phone Number`}</p>
                   <input
                     type="number"
-                    required
                     name="number"
                     placeholder="Enter your phone number"
-                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[47px]"
+                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[39px] md:h-[47px]"
                   />
                 </div>
                 <div>
-                  <p className="mb-[11px]">Email</p>
+                  <p className="mb-[6px] md:mb-[11px]">Email</p>
                   <input
                     type="text"
-                    required
                     name="mail"
                     placeholder="Enter your email address"
-                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[47px]"
+                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[39px] md:h-[47px]"
                   />
                 </div>
                 <div>
-                  <p className="mb-[11px]">Project Topic</p>
+                  <p className="mb-[6px] md:mb-[11px]">Project Topic</p>
                   <input
                     type="text"
-                    required
                     name="project_topic"
                     placeholder="What is your group project topic"
-                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[47px]"
+                    className="border-[1px] rounded-[4px] contact-box py-2 pl-2 w-[263px] h-[39px] md:h-[47px]"
                   />
                 </div>
-                <div>
+                {/* <div>
                   <p>Category</p>
-                </div>
+                </div> */}
               </div>
               <p className="text-[12px] italic text-[#FF26B9] mt-[23px] mb-[18px]">
                 Please review your registration details before submitting
@@ -97,7 +111,7 @@ const Register = () => {
                 I agreed with the event terms and conditions and privacy policy
               </div>
               <button
-                onClick={() => setShow(true)}
+                onClick={() => setOpenModal(true)}
                 className="register-button text-white w-full py-[17px] rounded-sm"
               >
                 Register Now
@@ -106,6 +120,7 @@ const Register = () => {
           </div>
         </div>
       </section>
+      {openModal && <Modal closeModal={setOpenModal} />}
     </Layout>
   );
 };
--- components/Modal.js
+++ components/Modal.js
@@ -3,35 +3,45 @@ import Yay from "../public/yay.png";
 import Good from "../public/big_check.png";
 import Image from "next/image";
 
-const Modal = () => {
+const Modal = ({ closeModal }) => {
   return (
-    <section className="w-[317px] h-[467px] rounded-sm">
-      <div className="relative">
-        <Image
-          src={Good}
-          width={174}
-          height={174}
-          alt="good"
-          className="absolute"
-        />
-        <Image
-          src={Yay}
-          width={184}
-          height={184}
-          alt="yay"
-          className="overflow-hidden"
-        />
-      </div>
-      <div>
-        <p className="w-[178px] mt-[26px] mb-[29px] text-[16px] font-semibold">
-          Congratulations you have successfully Registered!
-        </p>
-        <p>
-          Yes, it was easy and you did it! check your mail box for next step
-        </p>
-        <button>back</button>
-      </div>
-    </section>
+    <div className="">
+      <section className="modal left-0 top-0 absolute md:w-[699px] mx-auto md:h-[664px] md:left-[30%] md:top-[20%] w-[317px] h-min-[467px] rounded-sm z-5 text-white ">
+        <div className="mt-40">
+          <div className="relative">
+            <Image
+              src={Good}
+              width={174}
+              height={174}
+              alt="good"
+              className="absolute top-0 left-[20px] md:left-[190px]"
+            />
+            <Image
+              src={Yay}
+              width={184}
+              height={184}
+              alt="yay"
+              className="overflow-hidden mx-auto"
+            />
+          </div>
+          <div className="text-center">
+            <p className="md:w-[440px] w-[178px] mx-auto mt-[26px] mb-[29px] md:text-[20px] text-[16px] font-semibold">
+              Congratulations<br></br> you have successfully Registered!
+            </p>
+            <p>
+              Yes, it was easy and you did it!<br></br> check your mail box for
+              next step
+            </p>
+            <button
+              onClick={() => closeModal(false)}
+              className="register-button w-[200px] md:w-[600px] py-4 rounded-sm mt-5 font-semibold"
+            >
+              back
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
